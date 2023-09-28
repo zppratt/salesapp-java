f1d4f7ef01c9a94e68f4fbcaed706627fca460b3
Zach Pratt <zppratt@gmail.com>
2023-09-28 13:28:03 -0400

    init w/code

---
+++ src/main/java/Customer.java
@@ -0,0 +1,31 @@
+public class Customer {
+    private String firstName;
+    private String lastName;
+
+    // Constructor
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Customer customer1 = new Customer("Alice", "Onion");
+        System.out.println(customer1);
+
+        // JDBC URL, username, and password of PostgreSQL server
+        String url = "jdbc:postgresql://localhost:5432/salesapp";
+        String user = "postgres";
+        String password = "secret";
+
+        // Establish a connection
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected to the PostgreSQL server successfully.");
+            // Create a Statement
+            try (Statement statement = connection.createStatement()) {
+                // Execute a SELECT query
+                String sql = "SELECT * FROM customer";
+                try (ResultSet resultSet = statement.executeQuery(sql)) {
+                    // Process the result set
+                    while (resultSet.next()) {
+                        // Retrieve data from the result set
+                        int id = resultSet.getInt("id");
+                        String firstName = resultSet.getString("first_name");
+                        String lastName = resultSet.getString("last_name");
+
+                        // Print the data
+                        System.out.println("ID: " + id + ", First Name: " + firstName + ", Last Name: " + lastName);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Connection error: " + e.getMessage());
+        }
+    }
+}
